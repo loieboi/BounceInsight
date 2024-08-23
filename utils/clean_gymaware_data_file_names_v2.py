0928f6f943f49090824951f0739c9dd0a1543dcc
timos <timosp@student.ethz.ch>
2024-08-23 22:34:11 +0200

    upt: new notebooks and variable changes
    - better variables, similar to thesis
    - new notebooks for better overview

--- utils/clean_gymaware_data_file_names_v2.py
+++ utils/clean_gymaware_data_file_names_v2.py
@@ -56,8 +56,8 @@ def clean_and_reformat_data(file_path, output_file_path, participant_ids):
     # Rename the columns appropriately
     data.columns = [
         'Row Type', 'Rep Number', 't_ecc', 't_con', 't_total',
-        'F_turning', 'peak_F_con', 'mean_F_con', 'tpF', 'tpP', 'tpV', 'V_mean_ecc', 'P_peak_ecc', 'V_peak_ecc',
-        'P_mean_con', 'V_mean_con', 'P_peak_con', 'V_peak_con', 'dip_m', 'lD_m', 'hor_m'
+        'F_turning', 'peak_F_con', 'mean_F_con', 'tpF', 'tpP', 'tpV', 'mVe', 'pPe', 'pVe',
+        'mPc', 'mVc', 'pPc', 'pVc', 'dip_m', 'lD_m', 'hor_m'
     ]
 
     # Keep only the relevant rows (i.e., rows where 'Row Type' is 'Rep')
